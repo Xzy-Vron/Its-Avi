e72af2e3796c3198198a6a4e08ccec53a1cb6555
Avinash Ganore <avinashganore@gmail.com>
2025-09-03 03:58:34 +0530

    Create delete blog route

---
+++ app/api/edit/delete-blog/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbconnect from "@/lib/db-connect";
+import Blog from "@/models/blog.model";
+import User from "@/models/user.model";
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export async function DELETE(req: NextRequest) {
+
+  if (!SECRET_KEY || req.nextUrl.searchParams.get("key") !== SECRET_KEY) {
+    return Response.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    await dbconnect();
+
+    const id = "68b7591930c94f350c7ded04"; //👉 Enter your blog Id here to delete the blog
+
+    if (!id) return Response.json({ message: "Id not provided" }, { status: 404 });
+
+    await Blog.findByIdAndDelete(id);
+
+    await User.findOneAndUpdate(
+      { firstName: "Avinash" },
+      { $pull: { blogs: id } },
+      { new: true }
+    );
+
+    return Response.json({
+      message: "Blog deleted successfully",
+    });
+
+  } catch (error: any) {
+
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
